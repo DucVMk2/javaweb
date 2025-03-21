831ecc1035adacf6795fa7993e6cac727234d1e3
Minh Duc <ducm88778@gmail.com>
2025-03-21 15:26:19 +0700

    update search building

--- src/main/java/com/javaweb/api/BuildingAPI.java
+++ src/main/java/com/javaweb/api/BuildingAPI.java
@@ -1,6 +1,7 @@
 package com.javaweb.api;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -19,9 +20,9 @@ public class BuildingAPI {
 	private BuildingService buildingService;
 	
 	@GetMapping(value = "/api/building/")
-	public List<BuildingDTO> getBuilding(@RequestParam (name = "name", required = false) String name,
-			@RequestParam (name = "districtid", required = false) Long district) {
-		List<BuildingDTO> result = buildingService.findAll(name, district);
+	public List<BuildingDTO> getBuilding(@RequestParam Map<String, Object> params,
+										@RequestParam(name="typeCode") List<String> typeCode) {
+		List<BuildingDTO> result = buildingService.findAll(params, typeCode);
 		return result;
 	}
 	
--- src/main/java/com/javaweb/repository/entity/BuildingEntity.java
+++ src/main/java/com/javaweb/repository/entity/BuildingEntity.java
@@ -1,12 +1,68 @@
 package com.javaweb.repository.entity;
 
 public class BuildingEntity {
+	private Long id;
 	private String name;
+	private Long districtId;
 	private Integer numberOfBasement;
 	private String ward;
 	private String street;
+	private Long floorArea;
+	private Long rentPrice;
+	private String managerName;
+	private String managerPhonenumber;
+	private String serviceFee;
+	private Long brokerageFee;
 	public String getName() {
 		return name;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getDistrictId() {
+		return districtId;
+	}
+	public void setDistrictId(Long districtId) {
+		this.districtId = districtId;
+	}
+	public Long getFloorArea() {
+		return floorArea;
+	}
+	public void setFloorArea(Long floorArea) {
+		this.floorArea = floorArea;
+	}
+	public Long getRentPrice() {
+		return rentPrice;
+	}
+	public void setRentPrice(Long rentPrice) {
+		this.rentPrice = rentPrice;
+	}
+	public String getManagerName() {
+		return managerName;
+	}
+	public void setManagerName(String managerName) {
+		this.managerName = managerName;
+	}
+	public String getManagerPhonenumber() {
+		return managerPhonenumber;
+	}
+	public void setManagerPhonenumber(String managerPhonenumber) {
+		this.managerPhonenumber = managerPhonenumber;
+	}
+	public String getServiceFee() {
+		return serviceFee;
+	}
+	public void setServiceFee(String serviceFee) {
+		this.serviceFee = serviceFee;
+	}
+	public Long getBrokerageFee() {
+		return brokerageFee;
+	}
+	public void setBrokerageFee(Long brokerageFee) {
+		this.brokerageFee = brokerageFee;
 	}
 	public void setName(String name) {
 		this.name = name;
